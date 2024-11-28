42cebac74b6cf3216dcb53d0b33824bb76f50da1
Jolene Sutton <jolene.sutton@gmail.com>
2024-11-28 06:53:02 -0400

    Initial commit

---
+++ R/save_wsp.R
@@ -0,0 +1,16 @@
+#' Save workspace with script name and today's date
+#'
+#' Must execute from within script, not in console
+#' @import rstudioapi
+#' @param path Path to save the workspace
+#' @export
+save_wsp<-function(path){
+  nameScript <- function(){
+    pth <- rstudioapi::getActiveDocumentContext()$path
+    a <- unlist(strsplit(pth, "/"))
+    b<-(a[length(a)])
+    c<-gsub(".R", "_", b, fixed = TRUE)
+    return(paste("WS_",c,sep=""))
+  }
+  save.image(paste0(path,nameScript(),Sys.Date(),".RData"))
+}
