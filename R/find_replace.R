7c15e21688c2feb3b6fd1f7fb19c17f915fc8479
Jolene Sutton <jolene.sutton@gmail.com>
2025-01-13 08:01:31 -0400

    Function find_replace to accept user defined input for instances where there is no match

--- R/find_replace.R
+++ R/find_replace.R
@@ -2,8 +2,9 @@
 #'
 #' Performs a find replace on a vector by comparing to another vector. When no matches occur, the original value is retained.
 #' @param original The vector to search
-#' @param to.match A vector that possibly contain matches to original. Must be same length as to.replace
-#' @param to.replace A vector of values to use when a match is found between original and to.match. Must be same length as to.match
+#' @param to.find A vector that possibly contain matches to original. Must be same length as to.replace
+#' @param to.replace A vector of values to use when a match is found between original and to.find Must be same length as to.match
+#' @param no.match User defined value to return where there is no match. Default is to return original value.
 #' @return Returns a data frame showing the original vector and the replacement. If no match was found, the original be returned. Both columns are characters.
 #' @examples
 #' #E.g., Search "original" for "1", "2", "3", "4", and replace with "blue","green","purple","yellow"
@@ -11,11 +12,12 @@
 #' to.find<-c(1,2,3,4)
 #' to.replace<-c("blue","green","purple","yellow")
 #' find_replace(original,to.find,to.replace)
+#' find_replace(original,to.find,to.replace, no.match = 'NA')
 #' @export
-find_replace <- function(original,to.match,to.replace){#to.match and to.replace must have same lengths
+find_replace <- function(original,to.find,to.replace,no.match ='original'){#to.find and to.replace must have same lengths
   row<-array()
   codes<-array()
-  master<-cbind(to.match,to.replace)
+  master<-cbind(to.find,to.replace)
   for(i in 1:length(original)){
     if(length(which(master[,1] %in% original[i]))>0){
       row[i]<-(which(master[,1] %in% original[i]))
@@ -24,8 +26,13 @@ find_replace <- function(original,to.match,to.replace){#to.match and to.replace 
   }
   replacements<-codes[which(!is.na(row))]
   toreplace<-which(original %in% master[,1])
-  replacement<-original
+
+  ifelse(no.match =='original', replacement <- original, replacement<-rep(no.match,length(original)))
+
   replacement[toreplace]<-replacements
+  replacement[which(replacement=='NA')]<-NA
+
+
   replacement<-cbind(original,replacement)
   replacement[,1:2]
 }
