1e01f92bbd1d490da527c83c9ef796c9e398b242
Jolene Sutton <jolene.sutton@gmail.com>
2025-02-21 07:40:24 -0400

    update function 'bin_variable'

--- R/bin_variable.R
+++ R/bin_variable.R
@@ -3,21 +3,21 @@
 #' Useful before summarizing and plotting.
 #' @param x A vector
 #' @param bin.width The bin width. E.g., 20 will make each bin a maximum of 20 units wide
+#' @importFrom plyr round_any
 #' @return A data frame
 #' @examples
 #' x<-c(seq(1,23,1),seq(5,12,1))
 #' bin_variable(x,5)
 #' @export
 bin_variable<-function(x,bin.width){
-  df<-as.data.frame(x)
-  STARTS<-seq(min(x),max(x),by=bin.width)
-  STOPS<-STARTS+(bin.width-1)
+  STARTS<-seq(plyr::round_any(min(x), bin.width, floor),max(x),by=bin.width)
+  STOPS<-STARTS+bin.width
 
   grp<-NA
   for(i in 1:length(STARTS)){
-    index<-which(df$x%in%STARTS[i]:STOPS[i])
-    grp[index]<-paste0(min(df[index,'x']),' to ', max(df[index,'x']  ))
+    index<-STARTS[i]<=x&x<STOPS[i]
+    grp[index]<-paste0(STARTS[i],' - ', STOPS[i] )
   }
-  return(cbind.data.frame(grp))
+  return(grp)
 }
 
